6b106a4f0b87cd5b5eb588d2f1adaa15acad6293
ltv-Jindo <jindodino123@gmail.com>
2026-09-05 13:42:54 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -5,9 +5,20 @@ import os
 app = Flask(__name__)
 
 DISCORD_WEBHOOK = os.environ.get("DISCORD_WEBHOOK")
+DISCORD_OPEN = os.environ.get("DISCORD_OPEN")
 
 @app.route("/")
 def main():
+    if DISCORD_OPEN:
+        try:
+            requests.post(
+                DISCORD_OPEN,
+                json={"content": "🔔 Someone need contact!"},
+                timeout=5
+            )
+        except:
+            pass
+        
     return render_template("main.html")
 
 @app.route("/contact")
